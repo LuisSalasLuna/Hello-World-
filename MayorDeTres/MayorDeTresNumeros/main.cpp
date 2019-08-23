4fb9752ad291e6c3fd7f3e14c77055bfed30f90f
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2019-08-23 09:19:03 -0500

    Third commit

--- MayorDeTres/MayorDeTresNumeros/main.cpp
+++ MayorDeTres/MayorDeTresNumeros/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int a = 15;
+    int b = 6;
+    int c = 7;
+    if(a>b){
+        if(a>c){
+            cout<< a << " es el mayor"<<endl;}
+        else{
+            cout<< c << " es el mayor"<<endl;}
+    }
+    else{
+        if(b>c){
+            cout<< b <<" es el mayor"<<endl;}
+        else{
+            cout<< c <<" es el mayor"<<endl;}
+    }
+
 }
